64c8f14fdf93f24fe057e698bb84db26cf67775e
wonjae <hadnjswp@gmail.com>
2023-02-15 16:32:14 +0900

    view단 수정 및 명칭 수정

--- app/src/routers/globalRouter.js
+++ app/src/routers/globalRouter.js
@@ -1,14 +1,15 @@
+
 const express = require("express");
+const { output } = require("../controller/browseController");
 const {
   getQuestionList,
   postQuestion,
   home
 } = require("../controller/boardController");
-
 const globalRouter = express.Router();
 
-globalRouter.get("/", home);
-
+globalRouter.get("/board", home);
+globalRouter.get("/browse" , output.browse);
 // boardRouter.route("/").get(getQuestionList).post(postQuestion);
 
 module.exports = globalRouter;
