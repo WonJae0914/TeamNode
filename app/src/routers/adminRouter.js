77f59e1c63a3bd33813ee1a313f7740d52f08004
MarinKr <youngjoongil@gmail.com>
2023-02-20 10:43:20 +0900

    asdas
    asdas

--- app/src/routers/adminRouter.js
+++ app/src/routers/adminRouter.js
@@ -3,15 +3,13 @@ const admin = require("../controller/adminController");
 const adminRouter = express.Router();
 const upload = require("../utils/adminUtil")
 
-adminRouter.get('/home',admin.adminHome)
-adminRouter.get('/write',admin.adminWriteG)
-adminRouter.post('/write/add',upload,admin.adminWriteP)
-adminRouter.get('/list',admin.adminList)
-adminRouter.get('/detail/:id',admin.adminDetail)
-adminRouter.put('/detail/delete',admin.adminDelete)
-adminRouter.get('/edit/:id',admin.adminPutG)
-adminRouter.put('/edit',admin.adminPutP)
-
-
+adminRouter.get('/home', admin.adminHome)
+adminRouter.get('/write', admin.adminWriteG)
+adminRouter.post('/write/add', upload, admin.adminWriteP)
+adminRouter.get('/list', admin.adminList)
+adminRouter.get('/detail/:id', admin.adminDetail)
+adminRouter.put('/detail/delete', admin.adminDelete)
+adminRouter.get('/edit/:id', admin.adminPutG)
+adminRouter.put('/edit', admin.adminPutP)
 
 module.exports = adminRouter;
