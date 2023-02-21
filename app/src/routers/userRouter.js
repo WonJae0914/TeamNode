e1057f9096358da3144967a2d3449a979cc46fc1
jinyoung1439 <jinyoung9121@gmail.com>
2023-02-21 18:23:25 +0900

    userupdate

--- app/src/routers/userRouter.js
+++ app/src/routers/userRouter.js
@@ -8,38 +8,39 @@ const bcrypt = require('bcrypt');
 const userRouter = express.Router();
 
 userRouter.use(session({   // session 부여하는건 reuter에서 사용하는거니까 app.use 가 아닌 router.use 로 부여함
-  secret: 'admin', // 비밀코드 -> 세션을 만들 때 쓰는 비밀번호
+  secret: '비밀코드', // 비밀코드 -> 세션을 만들 때 쓰는 비밀번호
   resave: true, // true  :  변경사항이 없어도 session을 다시 저장
   saveUninitialized: false
 })); // false :  empty session obj가 쌓이는 걸 방지
 userRouter.use(passport.initialize());
 userRouter.use(passport.session());
 
-passport.use(
-  new LocalStrategy({ usernameField: 'id' }, async (id, pw, done) => {
-    try {
-      const user = await User.findOne({ id });
-      if (!user) {
-        return done(null, false, { message: '등록된 아이디가 없습니다.' });
+passport.use(new LocalStrategy({ usernameField: 'id', passwordField: 'pw' },
+      async (id, pw, done) => {
+        try {
+          const user = await User.findOne({ id });
+          if (!user) {
+            return done(null, false, { message: '등록된 아이디가 없습니다.' });
+          }
+          const match = await bcrypt.compare(pw, user.pw);
+          if (!match) {
+            return done(null, false, { message: '비번틀림', 'pw': user.pw });
+          }
+          return done(null, user);
+        } catch (err) {
+          console.error(err);
+          return done(err);
+        }
       }
-      const isMatch = await bcrypt.compare(pw, user.pw);
-      if (!isMatch) {
-        return done(null, false, { message: '비밀번호를 잘못입력하였습니다.' });
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
-
-passport.serializeUser((user, done) => {
+    )
+  );
+passport.serializeUser((user, done) => { //세션을 저장시키는 코드 (로그인 성공 시 발동)
   done(null, user.id);
 });
 
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser(async (id, done) => { //로그인한 유저의 세션 아이디를 바탕으로 개인정보를 DB에서 찾는 역할
   try {
-    const user = await User.findById(id);
+    const user = await User.findOne({ id: id });
     done(null, user);
   } catch (err) {
     done(err);
@@ -52,7 +53,10 @@ const {
   renderLogin,
   login,
   privacypolicy,
-  userdetail
+  logout,
+  userdetail,
+  updateuser,
+  removeuser
 } = require('../controller/userController');
 
 userRouter.get('/signup',renderSignup);
@@ -60,7 +64,10 @@ userRouter.get('/privacypolicy', privacypolicy); // 개인정보처리방침
 userRouter.post('/signup',signup);
 userRouter.get('/login',renderLogin);
 userRouter.post('/login',login);
+userRouter.get('/',logout)
 userRouter.get('/userpage', userdetail); // user상세정보
+userRouter.post('/userpage', updateuser);
+userRouter.post('/userpage', removeuser);
 
 
 // module.exports = passport;
