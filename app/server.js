d4393a88148280afc658de708e058a75e1f79804
jinyoung1439 <jinyoung9121@gmail.com>
2023-02-27 18:23:44 +0900

    css분리

--- app/server.js
+++ app/server.js
@@ -33,8 +33,11 @@ app.use(express.urlencoded({ extended: true }));
 // session, passport 미들웨어
 app.use(session({
     secret: '비밀코드', // 비밀코드 -> 세션을 만들 때 쓰는 비밀번호
-    resave: true, // true  :  변경사항이 없어도 session을 다시 저장
-    saveUninitialized: false, // false :  empty session obj가 쌓이는 걸 방지
+    resave: true, // true  :  resave는 세션이 수정되지 않은 경우에도 세션을 저장할 지 여부를 결정
+    saveUninitialized: false, // saveUninitialized는 초기화되지 않은 세션을 저장할 지 여부를 결정
+    cookie:{
+      maxAge:36000,
+    } // false :  empty session obj가 쌓이는 걸 방지
     // store: new Session({mongooseConnection: mongoose.connection }) // 이걸 쓰려면 근데 npm i conect-mongo 해줘야함
     // store에 mongoose.connection을 쓴 것은, 기존에 연결된 DB를 그대로 사용하겠다는 말입니다.
 }));
@@ -44,7 +47,7 @@ app.use(session({
   //app.use(session()) 코드 아래에 위치해야 한다는 말이다. 또, Cookie 나 Cookie-parser 미들웨어 다음에 작성해야 한다. 
 
 //라우팅 미들웨어 (제일 하단 고정)
-app.use("/",browse);
+app.use("/", browse);
 app.use("/board", isLoggedIn,boardRouter);
 app.use("/admin", admin);
 app.use("/", user);
--- app/src/routers/userRouter.js
+++ app/src/routers/userRouter.js
@@ -20,10 +20,10 @@ userRouter.get('/privacypolicy', privacypolicy); // 개인정보처리방침
 userRouter.post('/signup',signup);
 userRouter.get('/login',renderLogin);
 userRouter.post('/login',login);
-userRouter.get("/user", userRouter);
+// userRouter.get("/user", userRouter);
 userRouter.get('/userpage', isLoggedIn ,userdetail); // user상세정보
 userRouter.post('/userpage', isLoggedIn,updateuser); // user정보변경
 userRouter.get('/userpage/delete', removeuser); // user삭제
-userRouter.get('/logout',isLoggedIn ,logout)
+userRouter.get('/logout',isLoggedIn ,logout);
 
 module.exports = userRouter;
