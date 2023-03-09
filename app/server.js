479bf5c244b71451ad6fb5335b94827deb8de1cf
wonjae <hadnjswp@gmail.com>
2023-03-09 12:41:13 +0900

    머지용2

--- app/server.js
+++ app/server.js
@@ -9,7 +9,6 @@ const user = require("./src/routers/userRouter");
 const app = express();
 const methodOverride = require("method-override");
 const errorHandler = require("./src/utils/errorHandler");
-const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 
@@ -49,9 +48,6 @@ app.use(session({
   app.use(passport.session()); 
   // passport는 session을 사용하기 때문에 exporess-session 미들웨어 코드 다음에 작성해야한다.
   //app.use(session()) 코드 아래에 위치해야 한다는 말이다. 또, Cookie 나 Cookie-parser 미들웨어 다음에 작성해야 한다. 
-
-// 보안
-app.use(helmet());
 
 // 요청 데이터 제한
 const limiter = rateLimit({
